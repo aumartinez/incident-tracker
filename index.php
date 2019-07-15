db5bcedd98dd64b7909afd39c6408a52a3696cd9
Augusto Martinez <acm_asd@hotmail.com>
2019-07-15 15:10:35 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'inc/sess-validate.php'; ?>
+<!doctype html>
+<html lang="en">
+    <head>
+      <?php include 'temp/meta.php'; ?>
+      <title>Main CPanel</title>
+      <?php include 'temp/styles.php'; ?>
+      <?php include 'temp/top-scripts.php'; ?>
+    </head>
+    <body>
+        <h1 class="text-center">Content will go here</h1>
+        
+        <?php include 'temp/bottom-scripts.php'; ?>
+    </body>
+</html>
