4f0080ad59e30222f3a3c7f817cb3edcbb84dd6a
Augusto Martinez <acm_asd@hotmail.com>
2019-07-15 14:45:21 -0400

    Create styles.php

---
+++ temp/styles.php
@@ -0,0 +1,5 @@
+<!-- Stylesheets -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<link href="css/global.css" rel="stylesheet" type="text/css" />
+<?php echo "\n"; ?>
