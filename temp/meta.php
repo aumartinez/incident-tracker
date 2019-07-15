50639b2a41fbdfd591f9a88964e69d837975c7c2
Augusto Martinez <acm_asd@hotmail.com>
2019-07-15 14:43:19 -0400

    Create meta.php

---
+++ temp/meta.php
@@ -0,0 +1,6 @@
+<!-- Meta tags -->
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="robots" content="noindex, nofollow" />
+<?php echo "\n"; ?>
