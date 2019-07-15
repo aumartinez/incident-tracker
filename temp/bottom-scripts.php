7d5b0a0b1c4cad3da03dd540e35b6bd3715ae4ad
Augusto Martinez <acm_asd@hotmail.com>
2019-07-15 14:49:11 -0400

    Create bottom-scripts.php

---
+++ temp/bottom-scripts.php
@@ -0,0 +1,6 @@
+<!-- Bottom scripts -->
+<!-- JQuery -->
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<?php echo "\n"; ?>
